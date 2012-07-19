cd60564c1178aa9b1d83c5b59b92cd63bd039ea0
Matthew Marshall <mateomm@gmail.com>
2012-07-18 20:47:32 -0500

    reworked some of the spring confic, added test

---
+++ ventureprime-web/src/test/java/me/gotdo/vp3/web/test/SampleTest.java
@@ -0,0 +1,24 @@
+package me.gotdo.vp3.web.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
